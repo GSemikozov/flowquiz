ea5e3aa4b497967e8e5d0f1840705fa391a7f09c
hermansemykozov <semikozovgerman@gmail.com>
2021-01-25 23:29:34 +0200

    CHANGE: rm unused global declarations

--- src/types/global.ts
+++ src/types/global.ts
@@ -2,22 +2,6 @@ import { Client as FilestackClient } from "filestack-js/build/main/lib/client";
 
 declare global {
     // eslint-disable-next-line
-    interface Lang {
-        get: (
-            translationKey: string,
-            params?: { [key: string]: unknown },
-            locale?: string,
-        ) => string;
-        choice: (
-            translationKey: string,
-            count: number,
-            params?: { [key: string]: unknown },
-            locale?: string,
-        ) => string;
-    }
-    const Lang: Lang;
-
-    // eslint-disable-next-line
     interface Window {
         filestackClient?: FilestackClient;
         filestackSecurity?: { signature: string; policy: string };
